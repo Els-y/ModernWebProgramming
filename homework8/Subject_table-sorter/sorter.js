c2b8c308e024cf54734972f3fedcf0d4f9cd15ed
Els-y <344432376@qq.com>
2016-11-06 21:17:10 +0800

    homework8 css

--- homework8/Subject_table-sorter/sorter.js
+++ homework8/Subject_table-sorter/sorter.js
@@ -1,5 +1,5 @@
 function bindTable() {
-    $ths = $("thead th");
+    $ths = $("th");
     $ths.click(sortTable());
     if ($ths.find("caret").length == 0)
         $ths.append('<span class="caret"></span>');
@@ -8,32 +8,35 @@ function bindTable() {
 function sortTable() {
     var last_col = -1;
     var asc = -1;
+
     return function() {
         var $this = $(this);
         var col_num = $this.parent().children().index($this);
         var $table = $this.parents("table");
         var $trs = $table.find("tbody").find("tr");
+
         if ($table.find("thead").length === 0) Array.prototype.shift.call($trs);
         $trs = $trs.detach();
+
+        if (last_col !== col_num) asc = -1;
+        else asc *= -1;
 
         Array.prototype.sort.call($trs, function(first, second) {
             var fHtml = $(first).children().eq(col_num).html();
             var sHtml = $(second).children().eq(col_num).html();
             if (fHtml < sHtml) {
-                return (last_col === -1 || last_col !== col_num) ? asc : -asc;
+                return asc;
             } else if (fHtml > sHtml) {
-                return (last_col === -1 || last_col !== col_num) ? -asc : asc;
+                return -asc;
             } else {
                 return 0;
             }
-        })
-        
-        if (last_col === col_num) asc *= -1;
+        });
         last_col = col_num;
-        $table.append($trs);
-        
-        $table.find(".caret").removeClass("ascend").removeClass("descend");
-        $this.find(".caret").addClass(asc === -1 ? "ascend" : "descend");
+
+        $trs.removeClass("alternate").filter(":odd").addClass("alternate");
+        $table.append($trs).find(".active").removeClass("active").find(".caret").removeClass("ascend descend");
+        $this.addClass("active").find(".caret").addClass(asc === -1 ? "ascend" : "descend");
     }
 }
 
