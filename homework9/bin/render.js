d5228ae671487eae65e479f207a95940cb0a6d73
Els-y <344432376@qq.com>
2016-11-12 17:27:39 +0800

    homework9 delete render console.log

--- homework9/bin/render.js
+++ homework9/bin/render.js
@@ -1,7 +1,6 @@
 function render(html, object) {
     var match, reg = /#\{(\w+)\}/g;
     while (match = reg.exec(html)) {
-        console.log(match);
         html = html.replace(match[0], object[match[1]] ? object[match[1]] : "");
     }
     return html;
