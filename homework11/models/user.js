d26501804b84b15ab0db3b1d4833cdbe8bc7de96
Els-y <344432376@qq.com>
2016-12-16 20:37:58 +0800

    homework11 add server checkexist

--- homework11/models/user.js
+++ homework11/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
+var Promise = require('bluebird');
 
 var Schema = mongoose.Schema;
 var userScheMa = new Schema({
@@ -22,6 +23,36 @@ userScheMa.methods.encryptPassword = function() {
 
 userScheMa.methods.comparePassword = function(pwd) {
   return bcrypt.compareSync(pwd, this.password);
+};
+
+userScheMa.statics.checkExist = function(userInfo) {
+  var promiseList = [
+    this.findOne({username: userInfo.username}).exec(),
+    this.findOne({stuID: userInfo.stuID}).exec(),
+    this.findOne({phone: userInfo.phone}).exec(),
+    this.findOne({email: userInfo.email}).exec(),
+  ];
+
+  var message = {
+    "username": "Username already exists",
+    "stuID": "Student Number already exists",
+    "phone": "Phone already exists",
+    "email": "Email already exists",
+  };
+
+  return Promise.all(promiseList).spread(function(uname, uid, uphone, uemail) {
+    if (uname) {
+      return Promise.reject(message.username);
+    } else if (uid) {
+      return Promise.reject(message.stuID);
+    } else if (uphone) {
+      return Promise.reject(message.phone);
+    } else if (uemail) {
+      return Promise.reject(message.uemail);
+    } else {
+      return Promise.resolve();
+    }
+  });
 };
 
 module.exports = mongoose.model('users', userScheMa);
