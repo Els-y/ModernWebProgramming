ce55b037a6a398d5dc4404f0e1a8cfc6c1d809b3
Els-y <344432376@qq.com>
2017-01-25 21:50:40 +0800

    blog modify date && add readme

--- angular-express-blog/public/javascripts/app.js
+++ angular-express-blog/public/javascripts/app.js
@@ -1,5 +1,3 @@
-// 'use strict';
-
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ui.bootstrap', ]).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
--- angular-express-blog/public/javascripts/controllers.js
+++ angular-express-blog/public/javascripts/controllers.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /* Controllers */
 // posts
 function IndexCtrl($scope, $http, $rootScope, $filter) {
